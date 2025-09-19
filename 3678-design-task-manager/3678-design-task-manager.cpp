ee64e867155b9088a8d2d334ebf8314e512c07c6
Gaurika29062004 <154672036+Gaurika29062004@users.noreply.github.com>
2025-09-20 00:28:05 +0530

    Time: 160 ms (98.4%), Space: 342.1 MB (99.68%) - LeetHub

--- 3678-design-task-manager/3678-design-task-manager.cpp
+++ 3678-design-task-manager/3678-design-task-manager.cpp
@@ -1,68 +1,43 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Task {
-    int userId;
-    int taskId;
-    int priority;
-    Task() = default;
-    Task(int u, int t, int p) : userId(u), taskId(t), priority(p) {}
-    // We want the set to have highest priority first.
-    // If priorities tie, choose larger taskId first.
-    bool operator<(const Task &other) const {
-        if (priority != other.priority) return priority > other.priority;
-        return taskId > other.taskId;
-    }
-};
-
 class TaskManager {
-    // map: taskId -> (userId, priority)
-    unordered_map<int, pair<int,int>> mp;
-    // ordered set of tasks by (priority desc, taskId desc)
-    set<Task> s;
-
+    using int2=pair<int , int>; 
+    unordered_map<int, int2> mp;// taskID->(prioity, userId)
+    priority_queue<int2> pq; //(priority, taskID)
 public:
     TaskManager(vector<vector<int>>& tasks) {
-        for (auto &t : tasks) {
-            int user = t[0], id = t[1], pr = t[2];
-            mp[id] = {user, pr};
-            s.insert(Task(user, id, pr));
+        mp.reserve(tasks.size());
+        for(auto& t: tasks){
+            int  u=t[0], i=t[1], p=t[2];
+            mp[i]={p, u};
+            pq.emplace(p, i);
         }
     }
-
+    
     void add(int userId, int taskId, int priority) {
-        mp[taskId] = {userId, priority};
-        s.insert(Task(userId, taskId, priority));
+        mp[taskId]={priority, userId};
+        pq.emplace(priority, taskId);
     }
-
+    
     void edit(int taskId, int newPriority) {
-        auto it = mp.find(taskId);
-        if (it == mp.end()) return; // problem statement guarantees existence; defensive
-        int user = it->second.first;
-        int oldP = it->second.second;
-        // remove old Task from set
-        s.erase(Task(user, taskId, oldP));
-        // update map and set
-        it->second.second = newPriority;
-        s.insert(Task(user, taskId, newPriority));
+        mp[taskId].first=newPriority;
+        pq.emplace(newPriority, taskId);
     }
-
+    
     void rmv(int taskId) {
-        auto it = mp.find(taskId);
-        if (it == mp.end()) return; // defensive
-        int user = it->second.first;
-        int pr = it->second.second;
-        s.erase(Task(user, taskId, pr));
-        mp.erase(it);
+        mp[taskId].first=-1;
     }
-
+    
     int execTop() {
-        if (s.empty()) return -1;
-        auto it = s.begin();            // highest priority, then highest taskId
-        int user = it->userId;
-        int id = it->taskId;
-        s.erase(it);
-        mp.erase(id);
-        return user;
+        while (!pq.empty()) {
+            auto [p, i] = pq.top();
+            if (mp.count(i) && mp[i].first == p) {
+                pq.pop();
+                int u=mp[i].second;
+                mp.erase(i); // Remove task after execution
+                return u;
+            }
+            pq.pop(); // Remove
+        }
+        return -1; // No valid tasks to execute
+        
     }
 };
