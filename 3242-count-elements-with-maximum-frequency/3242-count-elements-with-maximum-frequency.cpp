815ba3f2cc18ceff22a8d09106668de697f20f54
Gaurika29062004 <154672036+Gaurika29062004@users.noreply.github.com>
2025-09-22 23:54:59 +0530

    Time: 0 ms (100%), Space: 23 MB (89.81%) - LeetHub

--- 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.cpp
+++ 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.cpp
@@ -5,11 +5,9 @@ public:
         unordered_map<int,int> mpp;
         for(auto it : nums){
             mpp[it]++;
+            maxfreq = max(maxfreq , mpp[it]);
         } 
         int count = 0;
-        for(auto it : mpp){
-            maxfreq = max(maxfreq , it.second);
-        }
         for(auto it : mpp){
             if(it.second == maxfreq){
                 count += it.second;
