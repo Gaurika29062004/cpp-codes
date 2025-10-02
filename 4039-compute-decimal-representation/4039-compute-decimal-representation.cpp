2df4ecfd18bae6cb2b2ce2e4fc3c1579f4be1c49
Gaurika29062004 <154672036+Gaurika29062004@users.noreply.github.com>
2025-10-03 03:13:25 +0530

    Time: 0 ms (100%), Space: 9.5 MB (83.34%) - LeetHub

---
+++ 4039-compute-decimal-representation/4039-compute-decimal-representation.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> decimalRepresentation(int n) {
+        vector<int> ans;
+        long long place = 1;
+        while(n>0){
+            long long digit = n%10;
+            if(digit!=0) {
+                ans.push_back(digit*place);
+            }
+            n /= 10;
+            place *= 10;
+        }
+        sort(ans.begin(),ans.end(),greater<int>());
+        return ans;
+    }
+};
