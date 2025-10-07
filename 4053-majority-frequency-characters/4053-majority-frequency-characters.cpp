7813cc9a0124568607c91cfdceec5ce4ba6b3b2d
Gaurika29062004 <154672036+Gaurika29062004@users.noreply.github.com>
2025-10-08 04:09:55 +0530

    Time: 2 ms (79.28%), Space: 10.4 MB (41%) - LeetHub

--- 4053-majority-frequency-characters/4053-majority-frequency-characters.cpp
+++ 4053-majority-frequency-characters/4053-majority-frequency-characters.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
     string majorityFrequencyGroup(string s) {
-    unordered_map<char,int> mp1;
-    int group = 0;
-    int j = 0;
-    string ans = "";
-    for(auto it : s){
-        mp1[it]++;
-    }
-    map<int,int> mp2;
-    for(auto it : mp1){
-        mp2[it.second]++;
-    }
-    for(auto it : mp2){
-        group = max(group,it.second);
-        if(it.second == group) j = it.first;
-    }
-    for(auto it : mp1){
-        if(it.second == j){
-            ans += it.first;
+        unordered_map<char, int> freq;
+        for (char c : s) freq[c]++;
+
+        unordered_map<int, vector<char>> group;
+        for (auto& [ch, f] : freq) group[f].push_back(ch);
+
+        int maxSize = 0, maxFreq = 0;
+        for (auto& [f, chars] : group) {
+            if (chars.size() > maxSize || (chars.size() == maxSize && f > maxFreq)) {
+                maxSize = chars.size();
+                maxFreq = f;
+            }
         }
-    }
-    return ans;
-    
+
+        string ans;
+        for (char c : group[maxFreq]) ans += c;
+        return ans;
     }
 };
