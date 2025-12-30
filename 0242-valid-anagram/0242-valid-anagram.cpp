df67a49edabc19d290c45c14c825f1087cd27b63
Gaurika29062004 <154672036+Gaurika29062004@users.noreply.github.com>
2025-12-30 17:08:12 +0530

    Time: 0 ms (100%), Space: 9.6 MB (73.51%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!= t.size()) return false;
+        vector<int> hash(26,0);
+        for(int i=0;i<s.size();i++){
+            hash[s[i]-'a']++;
+        }
+        for(int i=0;i<t.size();i++){
+                hash[t[i]-'a']--;
+        }
+        for(int i=0;i<hash.size();i++){
+            if(hash[i]!=0) return false;
+        }
+        return true;
+
+        }
+};
