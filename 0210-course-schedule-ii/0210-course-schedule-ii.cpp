9aa8f0b1572a609e8c19fb9efd51bf27ffb9945c
Gaurika29062004 <154672036+Gaurika29062004@users.noreply.github.com>
2026-02-07 19:38:22 +0530

    Time: 4 ms (56.31%), Space: 19 MB (30.33%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        vector<int> indegree(numCourses);
+        queue<int> q;
+            for(auto it : prerequisites){
+                int u = it[0];
+                int v = it[1];
+                adj[v].push_back(u);
+            }
+        for(int i=0;i<numCourses;i++){
+            for(auto it : adj[i]){
+                indegree[it]++;
+            }
+        }
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0) q.push(i);
+        }
+        vector<int> ans;
+        while(!q.empty()){
+            int node= q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it : adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0) q.push(it);
+            }
+        }
+        if(ans.size()!=numCourses) return {};
+        else return ans;
+    }
+};
